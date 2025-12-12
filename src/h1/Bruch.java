e7e8677dd94c538e0ffa51d287b42669a754d37d
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-12-12 09:27:33 +0100

    improve logic of ggT and shorten

--- src/h1/Bruch.java
+++ src/h1/Bruch.java
@@ -11,27 +11,33 @@ public class Bruch {
     private int ggT(int x, int y) {
         int dividend = Math.abs(x);
         int divisor = Math.abs(y);
+
         if (dividend == 0) {
             return divisor;
         }
-        int rest = -1;
-        while (rest != 0) {
-            rest = dividend % divisor;
-            if (rest != 0) {
-                dividend = divisor;
-                divisor = rest;
+        if (divisor == 0) {
+            return dividend;
+        }
+
+        while (true) {
+            int rest = dividend % divisor;
+            if (rest == 0) {
+                break;
             }
+            dividend = divisor;
+            divisor = rest;
         }
         return divisor;
     }
 
     public void shorten() {
         int ggt = ggT(zaehler, nenner);
+        if (ggt == 0) return;
         zaehler = zaehler / ggt;
         nenner = nenner / ggt;
     }
 
-    public boolean hasSameValueAs(Bruch b) {
+    public boolean hasSameValueAs(h1.Bruch b) {
         return (this.zaehler * b.nenner) == (this.nenner * b.zaehler);
     }
 }
